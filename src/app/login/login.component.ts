169f7f61801e22913a59f6542d9fcfc8efb00784
pranav <pranavverma550@gmail.com>
2019-05-23 06:52:08 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { AuthService } from './../auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form;
+  constructor(private fb: FormBuilder,
+    private myRoute: Router,
+    private auth: AuthService) {
+    this.form = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+  ngOnInit() {
+  }
+  login() {
+    if (this.form.valid) {
+      this.auth.sendToken(this.form.value.email)
+      this.myRoute.navigate(["home"]);
+    }
+  }
+}
